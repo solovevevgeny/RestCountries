792389b61b017ada7012a9309d4b57ed700f76ba
Solovev <evgeny.solovev@gmail.com>
2024-02-27 19:09:25 +0300

    error  page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,16 @@
 function NotFoundPage() {
-    return ( 
-        <>
-            <h1>Not found!</h1>
-        </>
-     );
+    const error = useRouteError();
+    console.error(error);
+  
+    return (
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    );
 }
 
 export default NotFoundPage;
